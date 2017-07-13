0be0b01b53a46b2e9e28947a9bcf61e5005d4b78
Tim Maina <oneuptim@Tims-MacBook-Pro.local>
2017-07-12 21:41:16 -0600

    upgrade Node.js version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,38 @@ import 'rxjs/add/operator/map';
 import { Pipe, PipeTransform } from '@angular/core';
 import {MdGridListModule} from '@angular/material';
 
+@Pipe({name: 'values'})
+export class ValuesPipe implements PipeTransform {
+    transform(value: any, args?: any[]): Object[] {
+        let keyArr: any[] = Object.keys(value),
+            dataArr = [],
+            keyName = args[0];
+
+        keyArr.forEach((key: any) => {
+            value[key][keyName] = key;
+            dataArr.push(value[key])
+        });
+
+        if(args[1]) {
+            dataArr.sort((a: Object, b: Object): number => {
+                return a[keyName] > b[keyName] ? 1 : -1;
+            });
+        }
+
+        return dataArr;
+    }
+
+  pipes: [ValuesPipe]
+	// directives: [NgFor, NgIf]
+    
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 
 @Injectable()
 
@@ -22,15 +48,17 @@ import {MdGridListModule} from '@angular/material';
 export class AppComponent {
   private _url:string = 'assets/data/monitoring.json';
   private _checksumsUrl:string = 'assets/data/versions.json';
-  resque = {};  
-  checksums = [];
-  queues = [];
-  workers = [];
-  resqueObj = {};
+  resque:any = {};  
+  checksums:any = [];
+  queues:any = [];
+  workers:any = [];
+  keys:any = [];
+  public redis:Object;
+  resqueObj:any = {};
   selectedItem:any = {};
-  selectedWorker = {};
-  isDarkTheme = true;
-  switchDetailsView = true;
+  selectedWorker:any = {};
+  isDarkTheme:any = true;
+  switchDetailsView:any = true;
 
 
   constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer, private dialog: MdDialog, private _http:Http) {
@@ -41,17 +69,20 @@ export class AppComponent {
     // Step 2
     this.getMonitoringData().subscribe((data) => {
       this.resque = data;
-      this.queues = data.monitoring_json.overview.queues.all_queues
-      this.workers = data.monitoring_json.overview.working.workers_info
-      console.log(this.workers);
-      this.resqueObj = data.monitoring_json.stats_overview.resque
+      console.log(this.resque);
+      this.queues = data.monitoring_json.overview.queues.all_queues;
+      this.workers = data.monitoring_json.overview.working.workers_info;
+      this.keys = data.monitoring_json.stats_overview.keys;
+      this.redis = data.monitoring_json.stats_overview.redis;
+      console.log(this.redis);
+      this.resqueObj = data.monitoring_json.stats_overview.resque;
       this.selectedItem = this.queues[0];
       this.selectedWorker = this.workers[0];
     });
 
     this.getChecksumsData().subscribe((data) => {
       this.checksums = data;
-      console.log(data);
+      // console.log(data);
     });
 
   } // End of constructor
